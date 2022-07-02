f13ecb39785228015b8230d4e8942264e9dbba87
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2022-07-02 22:38:05 +0200

    Enable -O3

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
-g++ -Wall -Wextra minispline.cpp -s `pkg-config sdl SDL_gfx --cflags --libs` -o minispline
+g++ -O3 -Wall -Wextra minispline.cpp -s `pkg-config sdl SDL_gfx --cflags --libs` -o minispline
 ./minispline
